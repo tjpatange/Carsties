4865f5c0faf8c3943b909e8a0f8ea4db566f29c6
Tejashri Patange <tejashripatange@gmail.com>
2024-03-05 12:23:29 +0100

    End of Section 3

---
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Entities;
+
+namespace SearchService;
+
+public class AuctionSvcHttpClient
+{
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
+
+    public AuctionSvcHttpClient(HttpClient httpClient, IConfiguration configuration)
+    {
+        _httpClient = httpClient;
+        _configuration = configuration;
+    }
+
+    public async Task<List<Item>> GetItemsForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, string>()
+            .Sort(x => x.Descending(x => x.UpdatedAt))
+            .Project(x => x.UpdatedAt.ToString())
+            .ExecuteFirstAsync();
+
+        return await _httpClient.GetFromJsonAsync<List<Item>>(_configuration["AuctionServiceUrl"] 
+            + "/api/auctions?date=" + lastUpdated);
+    }
+}
